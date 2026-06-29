00364f69deb8fafef4f1db2202685c8a1244f8e6
Parneel Bhakhri <hi@pbhak.dev>
2026-06-29 14:05:48 -0700

    rewrite to use astro redirects

---
+++ astro.config.mjs
@@ -0,0 +1,11 @@
+import { defineConfig } from 'astro/config';
+
+export default defineConfig({
+  redirects: {
+    '/attendance': 'https://forms.fillout.com/t/oGMSdYVcYyus',
+    '/discord': 'https://discord.gg/wusBcXetKe',
+    '/gamedev': 'https://sunnyvale.libcal.com/event/16698074',
+    '/insta': 'https://www.instagram.com/fhshackclub/',
+    '/interest': 'https://forms.fillout.com/t/b1fbhf6XZLus'
+  }
+});
